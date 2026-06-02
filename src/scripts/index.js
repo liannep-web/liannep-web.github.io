d4d039e7b7d478b31ed252aa695813ada461d4b6
Lianne <lianneperol@gmail.com>
2026-06-02 21:07:22 +0800

    Added contact section, footer. Updated about and projects info. Styling updates

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -47,51 +47,98 @@ import iconJira from '../assets/icons/icon_jira-75x75.svg';
 import iconMonday from '../assets/icons/icon_monday-250x75.svg';
 import iconLocation from '../assets/icons/icon-location.svg';
 import iconLink from '../assets/icons/icon-link.svg';
+import projectStories from '../assets/1projects_stories.png';
+import projectCardBenefits from '../assets/2projects_card-benefits.png';
+import projectSleekComponents from '../assets/3projects_sleek-components.png';
+import projectReuseComponents from '../assets/4projects_reusable-components.png';
+import projectSandbox from '../assets/5projects_sandbox.png';
+import projectBoostup from '../assets/6projects_boostup.png';
 
 
 function loadImages() {
     let logos = document.querySelectorAll('.js-logo-image');
     let locations = document.querySelectorAll('.js-location-icon');
     let links = document.querySelectorAll('.js-link-icon');
+    let behanceIcons = document.querySelectorAll('.js-icon-behance-icon');
+    let linkedinIcons = document.querySelectorAll('.js-icon-linkedin-icon');
+    let ninetynineIcons = document.querySelectorAll('.js-icon-99-icon');
+    let bgImages = document.querySelectorAll('.js-bg-image');
 
-    logos.forEach(logo => { // Loop for multiple image uses
-        logo.src = myLogo;
-    });
 
-    locations.forEach(loc => { // Loop for multiple image uses
-        loc.src = iconLocation;
-    });
+    // console.log(logos.length, locations.length, links.length);
 
-    links.forEach(link => { // Loop for multiple image uses
-        link.src = iconLink;
-    });
+    if (logos.length || locations.length || links.length || behanceIcons.length || linkedinIcons.length || ninetynineIcons.length || bgImages.length) {
+
+        logos.forEach(logo => { // Loop for multiple image uses
+            logo.src = myLogo;
+        });
+
+        locations.forEach(loc => { // Loop for multiple image uses
+            loc.src = iconLocation;
+        });
+
+        links.forEach(link => { // Loop for multiple image uses
+            link.src = iconLink;
+        });
+
+        behanceIcons.forEach(behance => { // Loop for multiple image uses
+            behance.src = iconBehance;
+        });
+
+        linkedinIcons.forEach(linkedin => { // Loop for multiple image uses
+            linkedin.src = iconLinkedin;
+        });
+
+        ninetynineIcons.forEach(ninetynine => { // Loop for multiple image uses
+            ninetynine.src = icon99;
+        });
+
+        bgImages.forEach(bg => { // Loop for multiple image uses
+            bg.src = bgImage;
+        });
+    }
+
     
-    document.querySelector('.js-icon-99-icon').src = icon99
-    document.querySelector('.js-icon-behance-icon').src = iconBehance
-    document.querySelector('.js-icon-linkedin-icon').src = iconLinkedin
-    document.querySelector('.js-bg-image').src = bgImage
-    document.querySelector('.js-svc-image').src = svcImage
-    document.querySelector('.js-profile-image').src = profileImage
-    document.querySelector('.js-html-icon').src = iconHtml
-    document.querySelector('.js-css-icon').src = iconCss
-    document.querySelector('.js-js-icon').src = iconJs
-    document.querySelector('.js-bootstrap-icon').src = iconBootstrap
-    document.querySelector('.js-sass-icon').src = iconSass
-    document.querySelector('.js-webpack-icon').src = iconWebpack
-    document.querySelector('.js-node-icon').src = iconNode
-    document.querySelector('.js-github-icon').src = iconGithub
-    document.querySelector('.js-wpengine-icon').src = iconWpengine
-    document.querySelector('.js-php-icon').src = iconPhp
-    document.querySelector('.js-ps-icon').src = iconPs
-    document.querySelector('.js-ai-icon').src = iconAi
-    document.querySelector('.js-figma-icon').src = iconFigma
-    document.querySelector('.js-wp-icon').src = iconWp
-    document.querySelector('.js-salesforce-icon').src = iconSalesforce
-    document.querySelector('.js-modx-icon').src = iconModx
-    document.querySelector('.js-shopify-icon').src = iconShopify
-    document.querySelector('.js-duda-icon').src = iconDuda
-    document.querySelector('.js-jira-icon').src = iconJira
-    document.querySelector('.js-monday-icon').src = iconMonday
+    const imageMap = {
+        '.js-icon-99-icon': icon99,
+        '.js-icon-behance-icon': iconBehance,
+        '.js-icon-linkedin-icon': iconLinkedin,
+        '.js-bg-image': bgImage,
+        '.js-svc-image': svcImage,
+        '.js-profile-image': profileImage,
+        '.js-html-icon': iconHtml,
+        '.js-css-icon': iconCss,
+        '.js-js-icon': iconJs,
+        '.js-bootstrap-icon': iconBootstrap,
+        '.js-sass-icon': iconSass,
+        '.js-webpack-icon': iconWebpack,
+        '.js-node-icon': iconNode,
+        '.js-github-icon': iconGithub,
+        '.js-wpengine-icon': iconWpengine,
+        '.js-php-icon': iconPhp,
+        '.js-ps-icon': iconPs,
+        '.js-ai-icon': iconAi,
+        '.js-figma-icon': iconFigma,
+        '.js-wp-icon': iconWp,
+        '.js-salesforce-icon': iconSalesforce,
+        '.js-modx-icon': iconModx,
+        '.js-shopify-icon': iconShopify,
+        '.js-duda-icon': iconDuda,
+        '.js-jira-icon': iconJira,
+        '.js-monday-icon': iconMonday,
+        '.js-project-stories' : projectStories,
+        '.js-project-card-benefits' : projectCardBenefits,
+        '.js-sleek-components' : projectSleekComponents,
+        '.js-reuse-components' : projectReuseComponents,
+        '.js-project-sandbox' : projectSandbox,
+        '.js-project-boostup' : projectBoostup
+
+    };
+
+    Object.entries(imageMap).forEach(([selector, src]) => {
+        const element = document.querySelector(selector);
+        if (element) element.src = src;
+    });
 
 
 }
